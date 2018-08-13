4b70458875d6f95020d598bbc03ed4560f0ee09d
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-13 19:14:56 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,14 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def search(name)
+    students = []
+    if Student.search.name.map{|s| s.name}
+      students << s
+    else
+      Student.all
+    end
+  end
+    
 end
